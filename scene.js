2b0e96866c3246ae591ecd7738fabbc43f36678e
natashaannn <natashaannlum@gmail.com>
2020-05-10 21:55:36 +0800

    Added bundle.js and tree.js

--- scene.js
+++ scene.js
@@ -1,23 +1,40 @@
-// var THREE = window.THREE = require('three');
-// require('three/examples/js/loaders/GLTFLoader');
-//
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+//Require js libraries
 var THREE = require('three');
+var GLTFLoader = require('three/examples/js/loaders/GLTFLoader');
+var OrbitControls = require('three-orbitcontrols');
 
+//Scene
 var scene = new THREE.Scene();
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//Camera Perspective
+var camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 100 );
+camera.position.set( 0, 5, 50 );
 
+//Renderer
 var renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.z = 5;
+//Orbitcontroller
+var controls = new OrbitControls( camera, renderer.domElement );
+controls.maxPolarAngle = 0.9 * Math.PI / 2;
+controls.update();
 
+//Ground
+var groundgeometry = new THREE.PlaneBufferGeometry( 100, 100 );
+var groundmat = new THREE.MeshPhongMaterial ( { color: 0xffffff} );
+var ground = new THREE.Mesh( groundgeometry, groundmat );
+ground.position.y = 0;
+ground.receiveshadow = true;
+ground.rotation.x = - Math.PI / 2.0;
+scene.add( ground );
+
+//Animate
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
+	controls.update();
 
 }
 animate();
--- tree.js
+++ tree.js
@@ -1,8 +1,18 @@
-var loader = new THREE.GLTFLoader();
+loader = new THREE.GLTFLoader();
+const textureLoader = new THREE.TextureLoader();
+loader.load( 'sapling_-01/scene.gltf', function ( gltf ) {
+	var model = gltf.scene;
+	model.traverse ( ( o ) => {
+			 if ( o.isMesh ) {
+				 var textures = ['Bark_Mat_baseColor', 'Bark_Mat_normal', 'leaf_Mat_baseColor','leaf_Mat_normal'];
+         for (x of textures) {
+					 string = 'sapling_-01/textures/'
+					 o.material = new THREE.MeshBasicMaterial({map: textureLoader.load(string.concat(x,'.png'))});
+         }
+       }
+	 });
 
-loader.load( 'sapling_-01\scene.gltf', function ( gltf ) {
-
-	scene.add( gltf.scene );
+	scene.add( model );
 
 }, undefined, function ( error ) {
 
